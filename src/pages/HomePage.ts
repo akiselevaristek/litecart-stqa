fabc692f8db67a6fae88744ea9cf169945a6478a
Alex Kiselev <akiselev@aristeksystems.com>
2026-03-17 23:40:05 +0300

    Implement Navigation component and update HomePage to utilize it

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -1,14 +1,17 @@
 import { Page } from '@playwright/test';
 import { BasePage } from '@pages/BasePage';
+import { Navigation } from '@components';
 import { URLS } from '@config';
 
 export class HomePage extends BasePage {
+  readonly navigation: Navigation;
 
-    constructor(page: Page) {
-        super(page);
+  constructor(page: Page) {
+    super(page);
+    this.navigation = new Navigation(page);
   }
 
   async goto() {
-    await this.page.goto(URLS.HOME)
+    await this.page.goto(URLS.HOME);
   }
 }
