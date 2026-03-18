3dd2a4dfdc44c81da1665ddd8a3fdc9ce3012494
Alex Kiselev <akiselev@aristeksystems.com>
2026-03-18 18:27:54 +0300

    Refactor order test: enhance step descriptions, add order success page verification, and improve product selection logic for better clarity and maintainability.

--- tests/order/order-without-discount.spec.ts
+++ tests/order/order-without-discount.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@fixtures';
+import { expect, test } from '@fixtures';
 
 test.describe('Login', () => {
   test.use({ useStorageState: true });
@@ -11,21 +11,37 @@ test.describe('Login', () => {
     homePage,
     productDetailsPage,
     checkoutPage,
+    orderSuccessPage,
   }) => {
-    const product = await homePage.getProductWithoutDiscount();
-    await product.locator.click();
-    await productDetailsPage.expectProductDetailsAre(
-      product.name, product.price, product.manufacturer, product.link
-    );
-    
-    await productDetailsPage.cart.expectQuantityIs(0);
-    await productDetailsPage.addToCart({ count: 3 });
-    await productDetailsPage.cart.expectQuantityIs(3);
-    await productDetailsPage.cart.clickCheckout();
+    await test.step('Логин с тестовой учеткой. Логин прошел успешно', async () => {
+      await expect(homePage.sidebarAccountBox.logoutLink).toBeVisible();
+    });
 
-    await checkoutPage.product.expectProductDetails(product.name, product.price, 3);
-    await checkoutPage.summary.expectProductInTheTable(product.name, 3);
-    await checkoutPage.summary.expectTotalPriceForOneProduct(product.price, 3);
-    await checkoutPage.confirmOrder();
+    const product = await test.step('Выбираем один товар без скидки. Товар выбран', async () => {
+      const selectedProduct = await homePage.getProductWithoutDiscount();
+      await selectedProduct.locator.click();
+      await productDetailsPage.expectProductDetailsAre(
+        selectedProduct.name, selectedProduct.price, selectedProduct.manufacturer, selectedProduct.link
+      );
+      return selectedProduct;
+    });
+
+    await test.step('Добавляем в корзину 3шт выбранного товара. Товары были добавлены в корзину', async () => {
+      await productDetailsPage.cart.expectQuantityIs(0);
+      await productDetailsPage.addToCart({ count: 3 });
+      await productDetailsPage.cart.expectQuantityIs(3);
+    });
+
+    await test.step('Осуществляем переход в корзину. Проверяем что кол-во продукта и цена соответствуют ожидаемому значению', async () => {
+      await productDetailsPage.cart.clickCheckout();
+      await checkoutPage.product.expectProductDetails(product.name, product.price, 3);
+      await checkoutPage.summary.expectProductInTheTable(product.name, 3);
+      await checkoutPage.summary.expectTotalPriceForOneProduct(product.price, 3);
+    });
+
+    await test.step('Подтверждение заказа. Заказ создан успешно', async () => {
+      await checkoutPage.confirmOrder();
+      await orderSuccessPage.expectSuccessfulOrderMessage();
+    });
   });
 });
