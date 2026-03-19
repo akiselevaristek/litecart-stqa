13967596f7dcae7d3dcdf09a1fc106f172e5b557
Alex Kiselev <akiselev@aristeksystems.com>
2026-03-19 19:07:13 +0300

    Refactor LoginPage and update login tests: change successful login message locator, add error message locator, and enhance tryToLogin method. Update test descriptions for clarity.

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -2,12 +2,16 @@ import { Page, Locator, expect } from '@playwright/test';
 import { LoginForm } from '@components';
 import { appConfig, URLS } from '@config';
 
+export type ErrorMessages = 'Wrong password or the account is disabled, or does not exist';
+
 export class LoginPage {
   readonly loginForm: LoginForm;
+  readonly error: Locator;
   readonly successfulLoginMessage: Locator;
 
   constructor(private readonly page: Page) {
-    this.successfulLoginMessage = this.page.getByText('You are now logged in');
+    this.successfulLoginMessage = this.page.locator('.notice.success');
+    this.error = this.page.locator('.notice.error');
     this.loginForm = new LoginForm(this.page.locator('#box-login'));
   }
 
@@ -18,6 +22,11 @@ export class LoginPage {
   async loginAs(email: string, password: string) {
     await this.loginForm.login(email, password);
     await expect(this.successfulLoginMessage).toBeVisible();
+  }
+
+  async tryToLogin(email: string, password: string, message: ErrorMessages) {
+    await this.loginForm.login(email, password);
+    await expect(this.error).toContainText(message);
   }
 
   async loginAsDefaultUser() {
--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -2,10 +2,21 @@ import { test } from '@fixtures';
 import { appConfig } from '@config';
 
 test.describe('Login', () => {
-  test('User can login from login page', {}, async ({ 
+  test('Пользователь может залогиниться в систему', {}, async ({ 
     loginPage,
   }) => {
     await loginPage.goto();
     await loginPage.loginAs(appConfig.email, appConfig.password);
+  });
+
+  test('Попытка логина с некорректным паролем', {}, async ({ 
+    loginPage,
+  }) => {
+    await loginPage.goto();
+    await loginPage.tryToLogin(
+      appConfig.email, 
+      'wrong-password', 
+      'Wrong password or the account is disabled, or does not exist'
+    );
   });
 });
